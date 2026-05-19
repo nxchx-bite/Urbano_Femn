b9cb04364f3ed524544bf94d25cd53dfd525b776
Tomas <to.guzmang@duocuc.cl>
2026-05-18 20:18:24 -0400

    Commit First UF

---
+++ src/main/java/UrbanoFemn/EV2/UrbanoFemnApplication.java
@@ -0,0 +1,13 @@
+package UrbanoFemn.EV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanoFemnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanoFemnApplication.class, args);
+	}
+
+}
