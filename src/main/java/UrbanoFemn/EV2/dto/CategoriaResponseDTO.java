d151c5b00f90ca1d0f63d67ac4e1827b5bc3ecb0
nxchx-bite <IGNA.TORRESP@DUOCUC.CL>
2026-05-24 14:57:16 -0400

    commit "CategoriaResponseDTO"

--- src/main/java/UrbanoFemn/EV2/dto/CategoriaResponseDTO.java
+++ src/main/java/UrbanoFemn/EV2/dto/CategoriaResponseDTO.java
@@ -1,5 +1,16 @@
 package UrbanoFemn.EV2.dto;
 
-public class CategoriaResponseDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoriaResponseDTO {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private Boolean activa;
 }
