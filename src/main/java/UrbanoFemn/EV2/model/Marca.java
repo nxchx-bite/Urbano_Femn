2562eb58f72797859cc7ba41270a6236564421c4
Tomás <to.guzmang@duocuc.cl>
2026-05-24 16:14:23 -0400

    Marca

--- src/main/java/UrbanoFemn/EV2/model/Marca.java
+++ src/main/java/UrbanoFemn/EV2/model/Marca.java
@@ -5,12 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// ═══════════════════════════════════════════════════
-// ENTIDAD · Marca.java
-// Representa una marca asociada a los productos.
-// Ejemplos: Shein, Zara, Urban Style, Fashion Chic.
-// Una marca puede estar asociada a muchos productos.
-// ═══════════════════════════════════════════════════
 
 @Data
 @NoArgsConstructor
@@ -23,19 +17,15 @@ public class Marca {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // Nombre de la marca.
     @Column(nullable = false, length = 100, unique = true)
     private String nombre;
 
-    // Descripción o información breve de la marca.
     @Column(length = 255)
     private String descripcion;
 
-    // País o procedencia de la marca.
     @Column(length = 100)
     private String paisOrigen;
 
-    // Permite activar o desactivar la marca
     @Column(nullable = false)
     private Boolean activa;
 }
