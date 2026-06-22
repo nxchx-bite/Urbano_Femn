6138940ffe5d3287b3989055e41a293c0ac0082d
nxchx-bite <IGNA.TORRESP@DUOCUC.CL>
2026-06-22 19:15:11 -0400

    fix(hateoas): corregir ProductoModelAssembler

--- src/main/java/UrbanoFemn/EV2/assemblers/ProductoModelAssembler.java
+++ src/main/java/UrbanoFemn/EV2/assemblers/ProductoModelAssembler.java
@@ -1,9 +1,23 @@
-<<<<<<< HEAD
-package UrbanoFemn.EV2.Assembler;
-=======
 package UrbanoFemn.EV2.assemblers;
->>>>>>> 6910f1f (feat(hateoas): implementar estructura de enlaces en la API)
 
-public class ProductoModelAssembler {
+import UrbanoFemn.EV2.controller.ProductoControllerV2;
+import UrbanoFemn.EV2.dto.ProductoResponseDTO;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@Component
+public class ProductoModelAssembler implements RepresentationModelAssembler<ProductoResponseDTO, EntityModel<ProductoResponseDTO>> {
+
+    @Override
+    public EntityModel<ProductoResponseDTO> toModel(ProductoResponseDTO producto) {
+        return EntityModel.of(producto,
+                linkTo(methodOn(ProductoControllerV2.class).obtenerPorId(producto.getId())).withSelfRel(),
+                linkTo(methodOn(ProductoControllerV2.class).obtenerTodos()).withRel("productos"),
+                linkTo(methodOn(ProductoControllerV2.class).buscarPorNombre(producto.getNombre())).withRel("buscar-por-nombre"),
+                linkTo(methodOn(ProductoControllerV2.class).buscarPorPrecioMaximo(producto.getPrecio())).withRel("buscar-por-precio")
+        );
+    }
 }
