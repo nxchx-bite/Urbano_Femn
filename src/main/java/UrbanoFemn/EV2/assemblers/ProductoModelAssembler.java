aafb33eac8b73ef7ca4e412e1f903e6343a90355
nxchx-bite <IGNA.TORRESP@DUOCUC.CL>
2026-06-22 18:19:15 -0400

    feat(hateoas): implementar estructura de enlaces en la API

--- src/main/java/UrbanoFemn/EV2/assemblers/ProductoModelAssembler.java
+++ src/main/java/UrbanoFemn/EV2/assemblers/ProductoModelAssembler.java
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 package UrbanoFemn.EV2.Assembler;
+=======
+package UrbanoFemn.EV2.assemblers;
+>>>>>>> 6910f1f (feat(hateoas): implementar estructura de enlaces en la API)
 
 public class ProductoModelAssembler {
 
