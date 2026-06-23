dfbe6eb977e99733b90e90076c8280a2a4fa0721
mairaquintanilla <mai.quintanilla@duocuc.cl>
2026-06-22 20:23:40 -0400

    ProductoControllerTest

--- src/test/java/UrbanoFemn/EV2/controller/ProductoControllerTest.java
+++ src/test/java/UrbanoFemn/EV2/controller/ProductoControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
